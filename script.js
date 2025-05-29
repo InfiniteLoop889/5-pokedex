d68d86ccaa4e97ac3a788265d6a810d08ef70374
InfiniteLoop889 <madgyver89@gmail.com>
2025-05-29 21:26:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+async function init() {
+  console.log(BASE_URL);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  init();
+});
