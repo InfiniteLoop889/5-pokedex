3c3959a648622301262d5a46e8210d5f442d9eff
InfiniteLoop889 <madgyver89@gmail.com>
2025-05-29 22:13:59 +0200

    - added get function

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ async function init() {
   console.log(BASE_URL);
 }
 
+// to implement:
+// GET
+// POST
+// PUT
+// DELETE
+
+async function getData(url) {
+  try {
+    let response = await fetch(url);
+    const responseToJson = await response.json();
+    console.log(responseToJson);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   init();
+  getData(BASE_URL);
 });
