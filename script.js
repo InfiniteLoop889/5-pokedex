95c77eaf2b6aabf9bbe56f7a8c39dbacad6e5d16
InfiniteLoop889 <madgyver89@gmail.com>
2025-05-31 15:27:27 +0200

    - added tailwind css and created basic static layout (later dynamic) - added fetchPokemonDetails() and displayData() for fetching correct urls and displaying the data

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon/ditto";
-
 async function init() {
-  console.log(BASE_URL);
+  const BASE_URL = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=8";
+  getData(BASE_URL);
 }
-
-// to implement:
-// GET
-// POST
-// PUT
-// DELETE
 
 async function getData(url) {
   try {
     let response = await fetch(url);
     const responseToJson = await response.json();
-    console.log(responseToJson);
+    const dataArray = responseToJson.results;
+    const detailedDataArray = [];
+
+    for (const pokemon of dataArray) {
+      const detailedData = await fetchPokemonDetails(pokemon.url);
+      detailedDataArray.push(detailedData);
+    }
+
+    displayData(detailedDataArray);
   } catch (error) {
     console.error(error);
   }
 }
 
+async function fetchPokemonDetails(url) {
+  try {
+    let response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function displayData(detailedDataArray) {
+  const cardTitleRef = document.querySelectorAll(".card-title");
+  const imgRef = document.querySelectorAll(".pokemon-img");
+
+  cardTitleRef.forEach((element, index) => {
+    element.innerText = detailedDataArray[index].name;
+  });
+  imgRef.forEach((element, index) => {
+    element.src = detailedDataArray[index].sprites.other["official-artwork"].front_default;
+    // element.src = detailedDataArray[index].sprites.other.home.front_default;
+    element.alt = detailedDataArray[index].name;
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   init();
-  getData(BASE_URL);
 });
