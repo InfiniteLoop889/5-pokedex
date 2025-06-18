6150278769ed700d14e44260190be78fe2f1413e
InfiniteLoop889 <madgyver89@gmail.com>
2025-06-18 22:44:37 +0200

    - reworked content of all files, simper appoach - overlay and search function still need to be finished

--- script.js
+++ script.js
@@ -1,169 +1,40 @@
-// ------------------------------- Global variables -------------------------------
-
-let offset = 0;
-let limit = 24;
-const allRenderedPokemonArr = [];
-let currentSearchState = {
+const state = {
+  offset: 0,
+  limit: 3,
+  allPokemon: [],
+  filteredPokemon: [],
   isFiltered: false,
-  filteredList: [],
-  searchString: "",
 };
 
-// ------------------------------- Fetch and display functions -------------------------------
-
-async function fetchPokemonData(url) {
-  try {
-    let response = await fetch(url);
-    let data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.error(error);
-  }
+async function fetchPokemonUrls(url) {
+  let response = await fetch(url);
+  let responseToJson = await response.json();
+  await fetchDetailedPokemonData(responseToJson.results);
 }
 
-async function createPokemonArray(dataArray, skipHandleSearch = true) {
-  const detailedDataArray = [];
-
-  for (const pokemon of dataArray) {
-    const detailedData = await fetchPokemonDetails(pokemon.url);
-    detailedDataArray.push(detailedData);
-    if (skipHandleSearch) {
-      allRenderedPokemonArr.push(detailedData);
-    }
+async function fetchDetailedPokemonData(pokemonUrls) {
+  let newPokemonarr = [];
+  for (const element of pokemonUrls) {
+    let response = await fetch(element.url);
+    let responseToJson = await response.json();
+    newPokemonarr.push(responseToJson);
+    state.allPokemon.push(responseToJson);
   }
-
-  return detailedDataArray;
+  renderPokemon(newPokemonarr);
 }
 
-async function fetchPokemonDetails(url) {
-  try {
-    const response = await fetch(url);
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function displayData(detailedDataArray, clearWrapper = false) {
+function renderPokemon(newPokemons) {
   const gridWrapperRef = document.getElementById("grid-wrapper");
+  let pokemonHtml = "";
 
-  if (clearWrapper) {
-    gridWrapperRef.innerHTML = "";
-  }
-
-  detailedDataArray.forEach((pokemon) => {
-    const pokemonCard = document.createElement("div");
-
-    pokemonCard.innerHTML = createPokemonCard(pokemon);
-
-    pokemonCard.addEventListener("click", () => {
-      openOverlay(pokemon, allRenderedPokemonArr);
-    });
-
-    gridWrapperRef.appendChild(pokemonCard);
+  newPokemons.forEach((pokemon) => {
+    const types = pokemon.types.map((type) => type.type.name);
+    pokemonHtml += createPokemonTemplate(pokemon, types);
   });
-}
 
-// ------------------------------- Load more button functions -------------------------------
-
-function addEventToLoadMoreButton() {
-  const buttonWrapper = document.getElementById("button-wrapper");
-  buttonWrapper.classList.remove("hidden");
-  buttonWrapper.addEventListener("click", loadMorePokemon);
-}
-
-async function loadMorePokemon() {
-  const buttonWrapper = document.getElementById("button-wrapper");
-  const loadingButton = document.getElementById("loading-button");
-
-  buttonWrapper.classList.add("hidden");
-  loadingButton.classList.remove("hidden");
-
-  try {
-    offset += limit;
-    const baseUrl = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
-    const rawPokemonData = await fetchPokemonData(baseUrl);
-    const detailedPokemonData = await createPokemonArray(rawPokemonData, true);
-    displayData(detailedPokemonData, false);
-
-    buttonWrapper.classList.remove("hidden");
-    loadingButton.classList.add("hidden");
-  } catch (error) {
-    console.error("Fehler beim Laden der Pokémon:", error);
-  }
-}
-
-// ------------------------------- Overlay functions -------------------------------
-
-async function openOverlay(pokemon, allRenderedPokemonArr) {
-  const overlay = document.createElement("div");
-  const listToUse = currentSearchState.isFiltered ? currentSearchState.filteredList : allRenderedPokemonArr;
-
-  overlay.classList.add("fixed", "top-0", "left-0", "w-full", "h-full", "bg-current/50", "flex", "justify-center", "items-center", "z-50");
-  overlay.innerHTML = await createOverlayTemplate(pokemon, listToUse);
-  document.body.appendChild(overlay);
-  document.body.classList.add("overflow-hidden");
-
-  addOverlayEventListeners(overlay, pokemon, listToUse);
-}
-
-function addOverlayEventListeners(overlay, pokemon, listToUse) {
-  const currentIndex = listToUse.findIndex((p) => p.id === pokemon.id);
-
-  addCloseButtonOverlayListener(overlay);
-  addCloseOverlayListener(overlay);
-  addPrevButtonListener(overlay, currentIndex, listToUse);
-  addNextButtonListener(overlay, currentIndex, listToUse);
-  addTabButtonListener();
-}
-
-function addCloseButtonOverlayListener(overlay) {
-  document.getElementById("close-overlay").addEventListener("click", () => {
-    document.body.removeChild(overlay);
-    document.body.classList.remove("overflow-hidden");
-  });
-}
-
-function addCloseOverlayListener(overlay) {
-  overlay.addEventListener("click", (event) => {
-    if (event.target === overlay) {
-      document.body.removeChild(overlay);
-      document.body.classList.remove("overflow-hidden");
-    }
-  });
-}
-
-function addPrevButtonListener(overlay, currentIndex, listToUse) {
-  document.getElementById("prev-button").addEventListener("click", () => {
-    const prevIndex = (currentIndex - 1 + listToUse.length) % listToUse.length;
-    document.body.removeChild(overlay);
-    openOverlay(listToUse[prevIndex], listToUse);
-  });
-}
-
-function addNextButtonListener(overlay, currentIndex, listToUse) {
-  document.getElementById("next-button").addEventListener("click", () => {
-    console.log(listToUse);
-    const nextIndex = (currentIndex + 1) % listToUse.length;
-    document.body.removeChild(overlay);
-    openOverlay(listToUse[nextIndex], listToUse);
-  });
-}
-
-function addTabButtonListener() {
-  document.addEventListener("click", (event) => {
-    if (event.target.classList.contains("tab-button")) {
-      const tab = event.target.dataset.tab;
-      document.querySelectorAll(".tab-button").forEach((button) => {
-        button.classList.add("border-transparent");
-      });
-      document.querySelectorAll(".tab-panel").forEach((panel) => {
-        panel.classList.add("hidden");
-      });
-      event.target.classList.remove("border-transparent");
-      document.getElementById(tab).classList.remove("hidden");
-    }
-  });
+  gridWrapperRef.innerHTML += pokemonHtml;
+  hideLoadingButton();
+  showLoadButton();
 }
 
 function getTypeElements(types) {
@@ -178,108 +49,51 @@ function getTypeElements(types) {
   return typeHtml;
 }
 
-function createPokemonCard(pokemon) {
-  const types = pokemon.types.map((typeObj) => typeObj.type.name);
-  return createPokemonTemplate(pokemon, types);
+function loadMorePokemon() {
+  state.offset += state.limit;
+  const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=${state.limit}&offset=${state.offset}`;
+  hideLoadButton();
+  showLoadingButton();
+  fetchPokemonUrls(baseUrl);
 }
 
-function processStats(stats) {
-  return stats.map((stat) => ({
-    name: capitalizeFirstLetter(stat.stat.name),
-    value: stat.base_stat,
-    width: Math.min(stat.base_stat, 150) / 1.5,
-  }));
+function renderSelectedPokemon(pokemonId) {
+  document.getElementById("overlay").innerText = pokemonId;
+  createOverlayTemplate(pokemonId);
+  openOverlay();
 }
 
-function createStatsTab(stats) {
-  const processedStats = processStats(stats);
-  return processedStats.map((stat) => createStatsTemplate(stat)).join("");
+function openOverlay() {
+  document.getElementById("overlay").classList.remove("hidden");
+  document.getElementById("overlay").classList.add("fixed");
+  document.body.classList.add("overflow-hidden");
 }
 
-function createTypesTab(types) {
-  let strengths = new Set();
-  let weaknesses = new Set();
-
-  types.forEach((type) => {
-    const typeInfo = typeChart[type];
-    if (typeInfo) {
-      typeInfo.strongAgainst.forEach((strong) => strengths.add(strong));
-      typeInfo.weakAgainst.forEach((weak) => weaknesses.add(weak));
-    }
-  });
-
-  return createTypesTemplate(strengths, weaknesses, types);
+function closeOverlay() {
+  document.getElementById("overlay").classList.add("hidden");
+  document.getElementById("overlay").classList.remove("fixed");
+  document.body.classList.remove("overflow-hidden");
 }
 
-async function loadEvolutions(pokemon) {
-  const speciesResponse = await fetch(pokemon.species.url);
-  const speciesToJson = await speciesResponse.json();
-  const evoResponse = await fetch(speciesToJson.evolution_chain.url);
-  const evoToJson = await evoResponse.json();
-
-  return evoToJson.chain;
+function onclickProtection(event) {
+  event.stopPropagation();
 }
 
-async function showPokemon(pokemon, allRenderedPokemonArr) {
-  const evolutionChain = await loadEvolutions(pokemon);
-  const evolutionTabHtml = createEvolutionsTab(evolutionChain, allRenderedPokemonArr);
-
-  return evolutionTabHtml;
+function showLoadButton() {
+  document.getElementById("button-wrapper").classList.remove("hidden");
 }
 
-function getEvolutionStages(evolutionChain, allRenderedPokemonArr) {
-  const stages = [];
-
-  const base = evolutionChain.species.name;
-  const basePokemon = findPokemonByName(base, allRenderedPokemonArr);
-  if (basePokemon) {
-    stages.push({
-      pokemon: basePokemon,
-      needsArrow: false,
-    });
-  }
-
-  evolutionChain.evolves_to.forEach((stage1, index1) => {
-    const stage1Pokemon = findPokemonByName(stage1.species.name, allRenderedPokemonArr);
-    if (stage1Pokemon) {
-      stages.push({
-        pokemon: stage1Pokemon,
-        needsArrow: index1 > 0 || basePokemon,
-      });
-
-      stage1.evolves_to.forEach((stage2, index2) => {
-        const stage2Pokemon = findPokemonByName(stage2.species.name, allRenderedPokemonArr);
-        if (stage2Pokemon) {
-          stages.push({
-            pokemon: stage2Pokemon,
-            needsArrow: index2 > 0 || stage1Pokemon,
-          });
-        }
-      });
-    }
-  });
-
-  return stages;
+function hideLoadButton() {
+  document.getElementById("button-wrapper").classList.add("hidden");
 }
 
-function createEvolutionsTab(evolutionChain, allRenderedPokemonArr) {
-  const stages = getEvolutionStages(evolutionChain, allRenderedPokemonArr);
-
-  const evolutionHtml = stages
-    .map((stage) => {
-      const elements = [];
-      if (stage.needsArrow) {
-        elements.push(createEvolutionArrowTemplate());
-      }
-      elements.push(createEvolutionTemplate(stage.pokemon));
-      return elements.join("");
-    })
-    .join("");
-
-  return evolutionHtml;
+function showLoadingButton() {
+  document.getElementById("loading-button").classList.remove("hidden");
 }
 
-// ------------------------------- Other help functions -------------------------------
+function hideLoadingButton() {
+  document.getElementById("loading-button").classList.add("hidden");
+}
 
 function capitalizeFirstLetter(val) {
   return String(val).charAt(0).toUpperCase() + String(val).slice(1);
@@ -300,21 +114,13 @@ function getTextColor(backgroundColor) {
   return brightness > 163 ? "dark:text-gray-800" : "text-white";
 }
 
-function findPokemonByName(name, allRenderedPokemonArr) {
-  return allRenderedPokemonArr.find((p) => p.name.toLowerCase() === name.toLowerCase());
-}
-
-// ------------------------------- init functions -------------------------------
+// ----------------------------------------------------------------------------------
 
 async function init() {
-  const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-  const dataArray = await fetchPokemonData(baseUrl);
-  const pokemonData = await createPokemonArray(dataArray, true);
-  displayData(pokemonData, true);
-  addEventToLoadMoreButton();
+  const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=${state.limit}&offset=${state.offset}`;
+  await fetchPokemonUrls(baseUrl);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   init();
-  initSearch();
 });
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,134 +1,78 @@
-// ------------------------------- Create Type Elements -------------------------------
-
 function createTypeElements(type, backgroundColor, textColor) {
   return `
-    <span class="min-w-[56px] px-2 py-1 rounded text-xs ${textColor} text-center" style="background-color: ${backgroundColor};">${capitalizeFirstLetter(type)}</span>
-  `;
+      <span class="min-w-[56px] px-2 py-1 rounded text-xs ${textColor} text-center" style="background-color: ${backgroundColor};">${capitalizeFirstLetter(type)}</span>
+    `;
 }
-
-// ------------------------------- Create Pokemon card -------------------------------
 
 function createPokemonTemplate(pokemon, types) {
   return `
-    <div class="max-w-xs border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm bg-white dark:bg-gray-800 transition-transform hover:scale-103 cursor-pointer">
-      <div class="p-4">
-        <h1 class="text-lg font-semibold text-gray-800 dark:text-gray-100">${capitalizeFirstLetter(pokemon.name)}</h1>
-      </div>
-      <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" class="w-full h-auto object-contain p-4" style="background-color: ${getBackgroundColor(pokemon)};"/>
-      <div class="pt-1 px-4 pb-4 space-y-2">
-        <h3 class="mb-3 text-sm text-gray-800 dark:text-gray-100">#${String(pokemon.id).padStart(3, "0")}</h3>
-        <div class="flex gap-2">
-          ${getTypeElements(types)}
+      <div class="max-w-xs border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm bg-white dark:bg-gray-800 transition-transform hover:scale-103 cursor-pointer" onclick="renderSelectedPokemon(${
+        pokemon.id
+      });">
+        <div class="p-4">
+          <h1 class="text-lg font-semibold text-gray-800 dark:text-gray-100">${capitalizeFirstLetter(pokemon.name)}</h1>
         </div>
-      </div>
-    </div>
-  `;
-}
-
-// ------------------------------- Create overlay tab components -------------------------------
-
-function createStatsTemplate(stat) {
-  return `
-    <div>
-      <div class="flex justify-between text-xs mb-1">
-        <span>${stat.name}</span>
-        <span>${stat.value}</span>
-      </div>
-      <div class="w-full bg-gray-200 rounded h-3 dark:bg-gray-700">
-        <div class="h-3 rounded bg-blue-500" style="width: ${stat.width}%;"></div>
-      </div>
-    </div>
-  `;
-}
-
-function createTypesTemplate(strengths, weaknesses, types) {
-  return `
-    <div class="space-y-4 px-4">
-      <div>
-        <h3 class="text-sm font-semibold">Types:</h3>
-        <p class="text-sm">${types.map((type) => capitalizeFirstLetter(type)).join(", ")}</p>
-      </div>
-      <div>
-        <h3 class="text-sm font-semibold">Strengths:</h3>
-        <p class="text-sm">${
-          Array.from(strengths)
-            .map((type) => capitalizeFirstLetter(type))
-            .join(", ") || "None"
-        }</p>
-      </div>
-      <div>
-        <h3 class="text-sm font-semibold">Weaknesses:</h3>
-        <p class="text-sm">${
-          Array.from(weaknesses)
-            .map((type) => capitalizeFirstLetter(type))
-            .join(", ") || "None"
-        }</p>
-      </div>
-    </div>
-  `;
-}
-
-// Evolution template functions
-function createEvolutionTemplate(pokemon) {
-  return `
-    <div class="flex flex-col items-center">
-      <img src="${pokemon.sprites.other["official-artwork"].front_default}" 
-           alt="${pokemon.name}" 
-           class="w-24 h-24 object-contain">
-      <span class="text-sm font-medium">${capitalizeFirstLetter(pokemon.name)}</span>
-    </div>
-  `;
-}
-
-function createEvolutionArrowTemplate() {
-  return `<span class="flex items-center justify-center text-gray-500">→</span>`;
-}
-
-// ------------------------------- Create overlay template -------------------------------
-
-async function createOverlayTemplate(pokemon, allRenderedPokemonArr) {
-  const evolutionTabHtml = await showPokemon(pokemon, allRenderedPokemonArr);
-  const types = pokemon.types.map((typeObj) => typeObj.type.name);
-
-  return `
-    <div class="max-w-sm h-auto border border-gray-200 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 text-center">
-      <h2 class="text-lg font-semibold text-gray-800 dark:text-gray-100 p-4">${capitalizeFirstLetter(pokemon.name)} <span class="text-gray-500">#${String(pokemon.id).padStart(3, "0")}</span></h2>
-      
-      <div class="relative">
-        <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" class="w-full h-auto object-contain p-12" style="background-color: ${getBackgroundColor(pokemon)};">
-        <button id="prev-button" 
-                class="absolute text-gray-800 left-0 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center cursor-pointer">
-          <img src="./img/icons8-back-50.png" alt="Previous" class="w-6 h-6 object-contain">
-        </button>
-        <button id="next-button" 
-                class="absolute text-gray-800 right-0 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center cursor-pointer">
-          <img src="./img/icons8-forward-50.png" alt="Next" class="w-6 h-6 object-contain">
-        </button>        
-      </div>
-      
-      <div class="grid grid-cols-3 bg-gray-200 dark:bg-gray-900">
-        <button class="tab-button py-2 border-b-2 border-gray-400 hover:border-gray-400 cursor-pointer text-center" data-tab="stats">Stats</button>
-        <button class="tab-button py-2 border-b-2 border-transparent border-gray-400 hover:border-gray-400 cursor-pointer text-center" data-tab="types">Types</button>
-        <button class="tab-button py-2 border-b-2 border-transparent border-gray-400 hover:border-gray-400 cursor-pointer text-center" data-tab="evolutions">Evolutions</button>
-      </div>
-
-      <div class="tab-content h-[180px] overflow-auto mt-4">
-        <div id="stats" class="tab-panel">
-          <div class="space-y-2 px-4">
-            ${createStatsTab(pokemon.stats)}
-          </div>
-        </div>
-        <div id="types" class="tab-panel flex justify-center items-center h-full hidden">
-          ${createTypesTab(types)}
-        </div>
-        <div id="evolutions" class="tab-panel flex justify-center items-center h-full hidden">
-          <div class="flex gap-x-4 justify-center items-center px-4">
-            ${evolutionTabHtml}
+        <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" class="w-full h-auto object-contain p-4" style="background-color: ${getBackgroundColor(
+    pokemon
+  )};"/>
+        <div class="pt-1 px-4 pb-4 space-y-2">
+          <h3 class="mb-3 text-sm text-gray-800 dark:text-gray-100">#${String(pokemon.id).padStart(3, "0")}</h3>
+          <div class="flex gap-2">
+            ${getTypeElements(types)}
           </div>
         </div>
       </div>
+    `;
+}
 
-      <button id="close-overlay" class="my-4">Close</button>
-    </div>
-  `;
+async function createOverlayTemplate(pokemon, pokemonList) {
+  // const evolutionTabHtml = await showPokemon(pokemon, pokemonList);
+  // const types = pokemon.types.map((typeObj) => typeObj.type.name);
+
+  return `
+      <div class="max-w-sm h-auto border border-gray-200 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 text-center">
+        <h2 class="text-lg font-semibold text-gray-800 dark:text-gray-100 p-4">${capitalizeFirstLetter(pokemon.name)} <span class="text-gray-500">#${String(pokemon.id).padStart(
+    3,
+    "0"
+  )}</span></h2>
+        
+        <div class="relative">
+          <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" class="w-full h-auto object-contain p-12" style="background-color: ${getBackgroundColor(
+    pokemon
+  )};">
+          <button id="prev-button" 
+                  class="absolute text-gray-800 left-0 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center cursor-pointer">
+            <img src="./img/icons8-back-50.png" alt="Previous" class="w-6 h-6 object-contain">
+          </button>
+          <button id="next-button" 
+                  class="absolute text-gray-800 right-0 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center cursor-pointer">
+            <img src="./img/icons8-forward-50.png" alt="Next" class="w-6 h-6 object-contain">
+          </button>        
+        </div>
+        
+        <div class="grid grid-cols-3 bg-gray-200 dark:bg-gray-900">
+          <button class="tab-button py-2 border-b-2 border-gray-400 hover:border-gray-400 cursor-pointer text-center" data-tab="stats">Stats</button>
+          <button class="tab-button py-2 border-b-2 border-transparent border-gray-400 hover:border-gray-400 cursor-pointer text-center" data-tab="types">Types</button>
+          <button class="tab-button py-2 border-b-2 border-transparent border-gray-400 hover:border-gray-400 cursor-pointer text-center" data-tab="evolutions">Evolutions</button>
+        </div>
+  
+        <!-- <div class="tab-content h-[180px] overflow-auto mt-4">
+          <div id="stats" class="tab-panel">
+            <div class="space-y-2 px-4">
+              ${createStatsTab(pokemon.stats)}
+            </div>
+          </div>
+          <div id="types" class="tab-panel flex justify-center items-center h-full hidden">
+            ${createTypesTab(types)}
+          </div>
+          <div id="evolutions" class="tab-panel flex justify-center items-center h-full hidden">
+            <div class="flex gap-x-4 justify-center items-center px-4">
+              ${evolutionTabHtml}
+            </div>
+          </div>
+        </div> -->
+  
+        <button id="close-overlay" class="my-4">Close</button>
+      </div>
+    `;
 }
